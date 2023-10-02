77890caa90e5909a44cf3c724a6d0fa564402ca4
Helmi Ikonen <helmi.ikonen@tuni.fi>
2023-10-02 12:29:34 +0300

    added example energy components to the demo page

--- frontend/src/components/Demo.jsx
+++ frontend/src/components/Demo.jsx
@@ -1,6 +1,10 @@
-import { Box, Button, Typography, Grid } from '@mui/material';
+import { Box, Button, Grid } from '@mui/material';
 import { useNavigate } from "react-router-dom";
 
+import SolarPowerIcon from '@mui/icons-material/SolarPower';
+import EnergyComponent from './EnergyComponent';
+import HeatPumpIcon from '@mui/icons-material/HeatPump';
+import ElectricCarIcon from '@mui/icons-material/ElectricCar';
 
 
 const Demo = () => {
@@ -13,15 +17,28 @@ const Demo = () => {
     spacing={4}
     columns={3}
     style={{ padding: '2vh' }}
+    
     >
       {/*Created container to span 2/3 columns. This is where the demo is.
         Replace "main view" text with the demos view. The color can be removed*/}
-      <Grid item xs={2}>
+      <Grid item xs={12} sm={2} minWidth='350px'>
         <Box 
           style={{padding: '2vh'}}
           bgcolor = "#cfe8fc" 
           height="96vh">
         Main view
+        <Grid container spacing={2}>
+          {/* container grid for the energy components */}
+          <Grid item xs={12} md={6}>
+            <EnergyComponent name="Solar panel" icon={<SolarPowerIcon />}/>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <EnergyComponent name="Heat pump" icon={<HeatPumpIcon />} />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <EnergyComponent name="Electric car" icon={<ElectricCarIcon />} />
+          </Grid>
+        </Grid>
         </Box>
       </Grid>
 
@@ -32,7 +49,7 @@ const Demo = () => {
 
           {/*Created container 2, where the current time and selected time range is shown
           Time text will be replaced*/}
-          <Grid item xs={1}>
+          <Grid item xs={1} minWidth='350px'>
             <Box 
               style={{padding: '2vh'}} 
               bgcolor = "#cfe8fc" 
@@ -43,7 +60,7 @@ const Demo = () => {
 
           {/*Created container 3, where the savings is shown
           Savings text will be replaced*/}
-          <Grid item xs={1}>
+          <Grid item xs={1} minWidth='350px'>
             <Box
               style={{padding: '2vh'}} 
               bgcolor = "#cfe8fc" 
--- frontend/src/components/EnergyComponent.jsx
+++ frontend/src/components/EnergyComponent.jsx
@@ -1,5 +1,30 @@
-const EnergyComponent = () => {
-  // this will represent a single energy component like solar panel
+import { Card, CardContent, CardActions, Typography } from '@mui/material';
+import { Button } from '@mui/material'; 
+
+const EnergyComponent = (props) => {
+  const { name, icon } = props;
+  return (
+    <Card sx={{
+      maxWidth: '500px',
+      minWidth: '250px',
+      minHeight: '20%',
+      padding: '2vh',
+      margin: '1vh'}}>
+      {icon} 
+      <Typography variant='body1' margin='1vh'>{name}</Typography>
+      <CardContent>
+        <Typography variant='body2'>
+          Energy consumption:
+          {/*or some other basic info about the component 
+          passed to the component as props*/}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        {/*this will open a bigger view of the energy component with more info*/}
+        <Button size='small'>Show more</Button>
+      </CardActions>
+    </Card>
+  )
 }
 
 export default EnergyComponent;
