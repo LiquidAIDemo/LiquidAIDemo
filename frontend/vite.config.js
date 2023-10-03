493acd6ebd46de6bd6c2db8ed105467e4c688c07
Tuomas <tuomas.2.makinen@tuni.fi>
2023-10-03 13:47:41 +0300

    resolved conflicts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { resolve } from "node:path";
+import vitePluginSvgr from 'vite-plugin-svgr';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vitePluginSvgr],
   optimizeDeps: {
     include: ['@emotion/styled']
   },
   esbuild: {
     jsxInject: `import React from 'react'`
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
   },
   server: {
     proxy: {
