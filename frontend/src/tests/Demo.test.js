4c41e1a02a64d2a4b6e411f9964a33d75c6e8220
Tuomas <tuomas.2.makinen@tuni.fi>
2023-10-10 20:21:35 +0300

    add more testing dependencies, demo component unit tests, mocks folder with react-water-wave mock

---
+++ frontend/src/tests/Demo.test.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import { render, screen, fireEvent } from '@testing-library/react'
+import Demo from '../components/Demo'
+import Welcome from '../components/Welcome'
+import { MemoryRouter, Route, Routes } from 'react-router-dom'
+
+test("renders content", () => {
+    render(
+      <MemoryRouter>
+        <Demo />
+      </MemoryRouter>
+    )
+    
+    expect(screen.getByText('Main view')).toBeInTheDocument()
+    expect(screen.getByText('Time')).toBeInTheDocument()
+    expect(screen.getByText('Savings')).toBeInTheDocument()
+    expect(screen.getByText('Stop')).toBeInTheDocument()
+    expect(screen.getByText('Restart')).toBeInTheDocument()
+})
+
+test('"stop" button navigates to welcome page', () => {
+    let navigate
+  
+    render(
+      <MemoryRouter initialEntries={['/demo']}>
+        <Routes>
+          <Route path="/demo" element={<Demo />} />
+          <Route path="/" element={<Welcome />} />
+        </Routes>
+      </MemoryRouter>
+    )
+    navigate = screen.getByText('Stop').navigate
+  
+    const stopButtonElement = screen.getByText('Stop')
+    fireEvent.click(stopButtonElement)
+    const welcomePageElement = screen.getByText("Welcome to Liquid AI Demo!")
+    expect(welcomePageElement).toBeInTheDocument()
+  })
+
