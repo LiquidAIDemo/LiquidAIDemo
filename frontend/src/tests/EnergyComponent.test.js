cf55821fea76657652d74f0c20536a77bfa6eba5
Tuomas <tuomas.2.makinen@tuni.fi>
2023-10-11 15:34:00 +0300

    fix EnergyComponent test

--- frontend/src/tests/EnergyComponent.test.js
+++ frontend/src/tests/EnergyComponent.test.js
@@ -2,14 +2,21 @@ import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import EnergyComponent from '../components/EnergyComponent'
+import { MemoryRouter } from 'react-router-dom'
 
 test("renders name, type and 'Show more' button", () => {
   const eComponent = {
+      id: 1,
       name: 'Test name',
-      type: 'Test type'
+      type: 'Test type',
+      desciption: 'Test description'
   }
   
-  render(<EnergyComponent {...eComponent} />)
+  render(
+    <MemoryRouter>
+      <EnergyComponent {...eComponent} />
+    </MemoryRouter>
+  )
 
   expect(screen.getByText('Test name')).toBeInTheDocument()
   expect(screen.getByText('Energy Test type')).toBeInTheDocument()
