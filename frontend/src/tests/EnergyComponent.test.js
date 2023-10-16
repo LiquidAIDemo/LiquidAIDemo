e8a1c53fc5710acfc78968df635ce0da41ac4dae
Tuomas <tuomas.2.makinen@tuni.fi>
2023-10-16 20:07:42 +0300

    show more button test

--- frontend/src/tests/EnergyComponent.test.js
+++ frontend/src/tests/EnergyComponent.test.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import EnergyComponent from '../components/EnergyComponent'
-import { MemoryRouter } from 'react-router-dom'
+import EnergyComponentPage from '../components/EnergyComponentPage'
+import { MemoryRouter, Route, Routes } from 'react-router-dom'
 
 test("renders content correctly", () => {
   const eComponent = {
       id: 1,
       name: 'Test name',
       type: 'Test type',
-      desciption: 'Test description'
+      description: 'Test description'
   }
   
   render(
@@ -22,5 +23,30 @@ test("renders content correctly", () => {
   expect(screen.getByText('Energy Test type')).toBeInTheDocument()
   expect(screen.getByText('Show more')).toBeInTheDocument()
   expect(screen.queryByText('Test description')).not.toBeInTheDocument()
+})
+
+test('"Show more" button navigates to component page', () => {
+
+  const eComponent = {
+    id: 1,
+    name: 'Test name',
+    type: 'Test type',
+    description: 'Test description'
+  }
+  const componentPagePath = `/component/${eComponent.id}`
+  
+  render(
+    <MemoryRouter initialEntries={['/demo']}>
+      <Routes>
+        <Route path="/demo" element={<EnergyComponent {...eComponent} />} />
+        <Route path={componentPagePath} element={<EnergyComponentPage />} />
+      </Routes>
+    </MemoryRouter>
+  )
+
+  const showMoreButtonElement = screen.getByText('Show more')
+  fireEvent.click(showMoreButtonElement)
+  const componentPageElement = screen.getByText(eComponent.description)
+  expect(componentPageElement).toBeInTheDocument()
 })
 
