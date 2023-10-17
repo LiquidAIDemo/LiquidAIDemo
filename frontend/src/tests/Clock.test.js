9f8ff7483abd746dab5279a83b88397564de6fcd
Tuomas <tuomas.2.makinen@tuni.fi>
2023-10-17 17:30:15 +0300

    fix clock tests

--- frontend/src/tests/Clock.test.js
+++ frontend/src/tests/Clock.test.js
@@ -3,12 +3,17 @@ import '@testing-library/jest-dom'
 import { render, screen, act, fireEvent } from '@testing-library/react'
 import Clock from '../components/Clock'
 
+const now = new Date()
+
 test("renders content", () => {
     render(<Clock />)
 
-    expect(screen.getByText(/Demo time:/)).toBeInTheDocument()
-    expect(screen.getByText(/Real time:/)).toBeInTheDocument()
+    expect(screen.getByText(/Select speed:/)).toBeInTheDocument()
+    expect(screen.getByText(/Select time range:/)).toBeInTheDocument()
+    expect(screen.getByText(/Demo:/)).toBeInTheDocument()
+    expect(screen.getByText(/Current/)).toBeInTheDocument()
     expect(screen.getByText('Pause')).toBeInTheDocument()
+    expect(screen.getByText('Reset')).toBeInTheDocument()
 })
 
 test("demo time runs correctly", () => {
@@ -17,8 +22,9 @@ test("demo time runs correctly", () => {
     act(() => {
         jest.advanceTimersByTime(3000)
     })
-    const demoTimeElement = screen.getByText(/Demo time:/)
-    expect(demoTimeElement).toHaveTextContent(/Demo time: 3:00/)
+    const demoTimeElement = screen.getByText(/Demo:/).parentElement
+    const currentHrs = now.getHours()
+    expect(demoTimeElement).toHaveTextContent(`Demo: ${currentHrs+3}:00`)
     jest.useRealTimers()   
 })
 
@@ -32,7 +38,8 @@ test("pause button pauses demo time", () => {
       jest.advanceTimersByTime(2000)
     })
     
-    const pausedDemoTime = screen.getByText(/Demo time:/)
-    expect(pausedDemoTime).toHaveTextContent(/Demo time: 0:00/)
+    const pausedDemoTime = screen.getByText(/Demo:/).parentElement
+    const currentHrs = now.getHours()
+    expect(pausedDemoTime).toHaveTextContent(`Demo: ${currentHrs}:00`)
     jest.useRealTimers()
 })
