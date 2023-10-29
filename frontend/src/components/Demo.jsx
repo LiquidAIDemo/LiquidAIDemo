2955ff67227e876df8e7dc48e1595700be235658
Helmi Ikonen <helmi.ikonen@tuni.fi>
2023-10-29 20:43:39 +0200

    added instructions/info dialog to the demo page

--- frontend/src/components/Demo.jsx
+++ frontend/src/components/Demo.jsx
@@ -1,4 +1,14 @@
-import { Box, Button, Grid } from '@mui/material';
+import { 
+  Box, 
+  Button, 
+  Grid, 
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle, 
+  Typography
+} from '@mui/material';
 import { useNavigate } from "react-router-dom";
 import { useState } from "react"
 import EnergyComponent from './EnergyComponent';
@@ -27,6 +37,52 @@ const Demo = () => {
     const newDemoTime = new Date(time);
     setDemoTime(newDemoTime);
   };
+
+  const [openInstructions, setOpenInstructions] = useState(false);
+
+  const handleCloseInstructions = () => {
+    setOpenInstructions(false);
+  }
+
+  const instructions = () => {
+    return (
+      <Dialog open={openInstructions} maxWidth={'sm'} fullWidth={true}>
+        
+          <DialogTitle sx={{display: 'flex', justifyContent: 'left', marginX: 2, marginTop: 2}}
+            >{"Liquid AI Demo"}
+          </DialogTitle>
+          <DialogContentText sx={{display: 'flex', justifyContent: 'center', marginX: 5}}>
+            The purpose of this demo is to show how energy consumption can be optimized in a
+            single-family house.
+          </DialogContentText>
+          <DialogTitle sx={{display: 'flex', justifyContent: 'left', marginX: 2, marginTop: 2}}
+            >{"Instructions for use"}
+          </DialogTitle>
+          <DialogContentText sx={{display: 'flex', justifyContent: 'left', marginX: 5}}>
+            You can select what components of the house are included in the demo from the Components
+            dropdown menu. You can inspect individual components by hovering over the component and 
+            clicking on it.<br/>
+            The demo shows the consumption and production of energy for each component and the price
+            of consumed energy by hour. The time range and speed of demo time can be changed from 
+            the dropdown menus.
+          </DialogContentText>
+          <DialogTitle sx={{display: 'flex', justifyContent: 'left', marginX: 2, marginTop: 2}}
+            >{"Source of the data"}
+          </DialogTitle>
+          <DialogContentText sx={{display: 'flex', justifyContent: 'left', marginX: 5}}>
+            Consumption and production data is currently made-up test data for demonstrating purposes.<br/>
+            The price data is fetched from Pörssisähkö API:
+          </DialogContentText>
+          <DialogContent sx={{display: 'flex', justifyContent: 'left', marginX: 2}}>
+            <a href="https://porssisahko.net">Link to the price API (the site is in Finnish)</a>
+          </DialogContent>
+          <DialogActions sx={{display: 'flex', justifyContent: 'center', margin: 2}}>
+            <Button variant='contained' onClick={handleCloseInstructions}>Back to demo</Button>
+          </DialogActions>
+
+      </Dialog>
+    );
+  }
 
   return (
     //Created container grid
@@ -332,15 +388,25 @@ const Demo = () => {
           </Grid>
 
           {/*Created container 4 for buttons and justify its content to the center*/}
-          <Grid item xs={1} style={{ display:"flex", justifyContent: "center" }}>
+          <Grid container style={{display: "flex", justifyContent: "center", alignItems: "center", margin: 10}}>
+            <Grid item xs={1} style={{minWidth: 100, margin: 5}}>
+            <Button 
+              variant="contained"
+              onClick={() => navigate("/")}>
+            Back
+            </Button>
+            </Grid>
+            <Grid item xs={1} style={{minWidth: 200, margin: 5}}>
+            <Button 
+              variant="contained"
+              onClick={() => setOpenInstructions(true)}>
+            More information
+            </Button>
+            </Grid>
+            {instructions()}
 
-          <Button 
-            variant="contained"
-            onClick={() => navigate("/")}>
-          Stop
-          </Button>
           </Grid>
-
+          
         </Grid>
            
       </Grid>
