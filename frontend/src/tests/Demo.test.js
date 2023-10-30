2cfb07bc2465d1f611715dfb4a217043f129806f
Milla Valio <milla.valio@tuni.fi>
2023-10-30 09:32:26 +0200

    Cancel auto format changes

--- frontend/src/tests/Demo.test.js
+++ frontend/src/tests/Demo.test.js
@@ -1,49 +1,48 @@
-import "@testing-library/jest-dom";
-import { render, screen, act } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import Demo from "../components/Demo";
-import Welcome from "../components/Welcome";
-import { MemoryRouter, Route, Routes } from "react-router-dom";
-import axios from "axios";
-import MockAdapter from "axios-mock-adapter";
+import '@testing-library/jest-dom'
+import { render, screen, act } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Demo from '../components/Demo'
+import Welcome from '../components/Welcome'
+import { MemoryRouter, Route, Routes } from 'react-router-dom'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
 
-const axiosMock = new MockAdapter(axios);
+const axiosMock = new MockAdapter(axios)
 
 test("renders content", async () => {
-  axiosMock.onGet("/api").reply(200, [{ price: 0 }]);
-  await act(async () => {
+  axiosMock.onGet('/api').reply(200, [{ price: 0 }])
+  await act( async () => {
     render(
       <MemoryRouter>
         <Demo />
       </MemoryRouter>
-    );
-  });
-
-  expect(screen.getByText("Components")).toBeInTheDocument();
-  expect(screen.getByText("Time")).toBeInTheDocument();
-  expect(screen.getByText("Savings")).toBeInTheDocument();
-  expect(screen.getByText("Back")).toBeInTheDocument();
-  expect(screen.getByText("More information")).toBeInTheDocument();
-});
+    )
+  })
+  
+  expect(screen.getByText('Components')).toBeInTheDocument()
+  expect(screen.getByText('Time')).toBeInTheDocument()
+  expect(screen.getByText('Savings')).toBeInTheDocument()
+  expect(screen.getByText('Back')).toBeInTheDocument()
+  expect(screen.getByText('More information')).toBeInTheDocument()
+})
 
 test('"back" button navigates to welcome page', async () => {
-  axiosMock.onGet("/api").reply(200, [{ price: 0 }]);
-  await act(async () => {
+  axiosMock.onGet('/api').reply(200, [{ price: 0 }])
+  await act( async () => {
     render(
-      <MemoryRouter initialEntries={["/demo"]}>
+      <MemoryRouter initialEntries={['/demo']}>
         <Routes>
           <Route path="/demo" element={<Demo />} />
           <Route path="/" element={<Welcome />} />
         </Routes>
       </MemoryRouter>
-    );
-  });
+    )
+  })
+  
+  const user = userEvent.setup({delay: null})
+  const stopButtonElement = screen.getByText('Back')
+  await user.click(stopButtonElement)
+  const welcomePageElement = screen.getByText("Welcome to Liquid AI demonstrator!")
+  expect(welcomePageElement).toBeInTheDocument()
+})
 
-  const user = userEvent.setup({ delay: null });
-  const stopButtonElement = screen.getByText("Back");
-  await user.click(stopButtonElement);
-  const welcomePageElement = screen.getByText(
-    "Welcome to Liquid AI demonstrator!"
-  );
-  expect(welcomePageElement).toBeInTheDocument();
-});
