dcbea6725eadff4ea5dd7f8aa1140110a3595a57
Tuomas <tuomas.2.makinen@tuni.fi>
2023-11-05 19:46:49 +0200

    wip

--- frontend/src/components/ElectricityPrice.jsx
+++ frontend/src/components/ElectricityPrice.jsx
@@ -22,14 +22,24 @@ const Chart = ({ consumptionData }) => {
         xAxis={[
           { 
             data: consumptionData.map(entry => entry.hour),
-            scaleType: 'time' 
+            scaleType: 'time',
+            hideTooltip: true,
           }
         ]}
         series={[
           {
             data: consumptionData.map(entry => entry.total),
+            area: true,
+            curve: 'natural',
+            showMark: false,
+            label: 'cents'
           },
-        ]}        
+        ]}
+        sx={{
+          '& .MuiAreaElement-root': {
+            fillOpacity: 0.4,
+          },
+        }}        
       />
     )
   }
@@ -40,9 +50,10 @@ const ElectricityPrice = ({ demoTime, demoPassedHrs }) => {
   const [prices, setPrices] = useState([])
   const [currentPrice, setCurrentPrice] = useState(0)
   const [currentConsumption, setCurrentConsumption] = useState(0)
-  const componentsData = energyComponents.components
+  const [componentsData] = useState(energyComponents.components)
   const [consumptionPerHour, setConsumptionPerHour] = useState({})
-  const [consumptionData, dispatchConsumptionData] = useReducer(consumptionDataReducer, [])    
+  const [consumptionData, dispatchConsumptionData] = useReducer(consumptionDataReducer, [])
+  //const [consumptionDataLoaded, setConsumptionDataLoaded] = useState(false)   
   
   useEffect(() => {    
     try {
@@ -71,21 +82,23 @@ const ElectricityPrice = ({ demoTime, demoPassedHrs }) => {
   }, [componentsData])
 
   useEffect(() => {
-    resetConsumptionData(demoPassedHrs)
-    const demoTimeCopy = new Date(demoTime.setMinutes(0, 0))
-    const total = consumptionPerHour[demoTime.getHours()]
-    const formattedDemoTime = demoTimeCopy.toLocaleString("fi-FI", { timeZone: "Europe/Helsinki" })
-    const priceObj = prices.find(priceObj => priceObj.startDate == formattedDemoTime)
-    
-    if (priceObj) {
-      setCurrentPrice(priceObj.price)
-      setCurrentConsumption(total)
-      dispatchConsumptionData({ type: 'ADD_CONSUMPTION_DATA', payload: { hour: demoTimeCopy, total: total * priceObj.price } })
+    if (prices.length > 0 && demoTime && demoPassedHrs >= 0 && consumptionPerHour) {
+      resetConsumptionData(demoPassedHrs)
+      const demoTimeCopy = new Date(demoTime.setMinutes(0, 0))
+      const total = consumptionPerHour[demoTime.getHours()]
+      const formattedDemoTime = demoTimeCopy.toLocaleString("fi-FI", { timeZone: "Europe/Helsinki" })
+      const priceObj = prices.find(priceObj => priceObj.startDate == formattedDemoTime)
+      
+      if (priceObj) {
+        setCurrentPrice(priceObj.price)
+        setCurrentConsumption(total)
+        dispatchConsumptionData({ type: 'ADD_CONSUMPTION_DATA', payload: { hour: demoTimeCopy, total: total * priceObj.price } })
+        //setConsumptionDataLoaded(true)
+      }
     }
   }, [prices, demoTime, demoPassedHrs, consumptionPerHour]);
   
   const resetConsumptionData = (demoPassedHrs) => {
-    console.log("call")
     if (demoPassedHrs === 0) {
       dispatchConsumptionData({ type: 'RESET_CONSUMPTION_DATA' })
     }
