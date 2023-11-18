0e57715d252b34ff855b89e4d9b2caece88ec0f7
Tuomas <tuomas.2.makinen@tuni.fi>
2023-11-18 18:49:37 +0200

    add tests for Demo component elements

--- frontend/src/tests/Demo.test.js
+++ frontend/src/tests/Demo.test.js
@@ -8,6 +8,7 @@ import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 
 const axiosMock = new MockAdapter(axios)
+const user = userEvent.setup({delay: null})
 
 test("renders content", async () => {
   axiosMock.onGet('/api').reply(200, [{ price: 0 }])
@@ -38,10 +39,107 @@ test('"back" button navigates to welcome page', async () => {
     )
   })
   
-  const user = userEvent.setup({delay: null})
   const stopButtonElement = screen.getByText('Back')
   await user.click(stopButtonElement)
   const welcomePageElement = screen.getByText("Welcome to LiquidAI demonstrator")
   expect(welcomePageElement).toBeInTheDocument()
 })
 
+test('components menu initializes correctly and checkboxes work correctly', async () => {
+  axiosMock.onGet('/api').reply(200, [{ price: 0 }])
+  await act( async () => {
+    render(
+      <MemoryRouter>
+        <Demo />
+      </MemoryRouter>
+    )
+  })
+
+  const componentsMenuElement = screen.getByTestId("ExpandMoreIcon")
+  await user.click(componentsMenuElement)
+  const checkboxes = [
+    screen.getByLabelText('Heat Pump'),
+    screen.getByLabelText('Electric board'),
+    screen.getByLabelText('Freezer'),
+    screen.getByLabelText('Heater'),
+    screen.getByLabelText('Hot water heater'),
+    screen.getByLabelText('Stove'),
+    screen.getByLabelText('Washing machine'),
+    screen.getByLabelText('Electric car 1'),
+    screen.getByLabelText('Electric car 2'),
+    screen.getByLabelText('Solar panel 1'),
+    screen.getByLabelText('Solar panel 2'),
+    screen.getByLabelText('Solar panel 3'),
+    screen.getByLabelText('Solar panel 4'),
+    screen.getByLabelText('Jacuzzi'),
+  ]
+  
+  // Check if all checkboxes are checked by default
+  checkboxes.forEach(cb => {
+    expect(cb).toBeChecked()
+  })
+  // Test checkbox functionality with Heat Pump
+  const heatPumpCheckbox = screen.getByLabelText("Heat Pump")
+  expect(heatPumpCheckbox.checked).toBe(true)
+  await user.click(heatPumpCheckbox)
+  expect(heatPumpCheckbox.checked).toBe(false)
+})
+
+test('"Clear all" and "Reset to default" buttons work correctly', async () => {
+  axiosMock.onGet('/api').reply(200, [{ price: 0 }])
+  await act( async () => {
+    render(
+      <MemoryRouter>
+        <Demo />
+      </MemoryRouter>
+    )
+  })
+
+  const componentsMenuElement = screen.getByTestId("ExpandMoreIcon")
+  await user.click(componentsMenuElement)
+  const checkboxes = [
+    screen.getByLabelText('Heat Pump'),
+    screen.getByLabelText('Electric board'),
+    screen.getByLabelText('Freezer'),
+    screen.getByLabelText('Heater'),
+    screen.getByLabelText('Hot water heater'),
+    screen.getByLabelText('Stove'),
+    screen.getByLabelText('Washing machine'),
+    screen.getByLabelText('Electric car 1'),
+    screen.getByLabelText('Electric car 2'),
+    screen.getByLabelText('Solar panel 1'),
+    screen.getByLabelText('Solar panel 2'),
+    screen.getByLabelText('Solar panel 3'),
+    screen.getByLabelText('Solar panel 4'),
+    screen.getByLabelText('Jacuzzi'),
+  ]
+  
+  const clearAllButton = screen.getByText("Clear all")
+  await user.click(clearAllButton)
+
+  checkboxes.forEach(cb => {
+    expect(cb).not.toBeChecked()
+  })
+
+  const resetButton = screen.getByText("Reset to default")
+  await user.click(resetButton)
+
+  checkboxes.forEach(cb => {
+    expect(cb).toBeChecked()
+  })
+})
+
+test('"Information" button works correctly', async () => {
+  axiosMock.onGet('/api').reply(200, [{ price: 0 }])
+  await act( async () => {
+    render(
+      <MemoryRouter>
+        <Demo />
+      </MemoryRouter>
+    )
+  })
+
+  const informationButton = screen.getByText("Information")
+  await user.click(informationButton)
+  expect(screen.getByText("Instructions for use")).toBeInTheDocument()
+})
