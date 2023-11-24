4fe927cad81ee8971b7b2c7f459deb941dceb667
Tuomas <tuomas.2.makinen@tuni.fi>
2023-11-24 18:48:54 +0200

    eslint config changes

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,13 +1,13 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, jest: true, cypress: true },
+  env: { browser: true, es2020: true, "jest/globals": true, "cypress/globals": true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'coverage', 'main.jsx'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'coverage', 'main.jsx', 'cypress.config.js'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh', 'react', 'jest', 'cypress'],
